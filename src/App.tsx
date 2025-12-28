4768298979635491a797478286529b5b1d8ebf68
Fernando Florenzano <f@designsystems.international>
2025-12-28 12:07:45 -0300

    time scale

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,11 @@ import "./App.css";
 interface Node extends d3.SimulationNodeDatum {
   fact: string;
   verification: string;
+  source: {
+    medium?: string;
+    platform?: string;
+  };
+  date: string | null;
   verificationRaw: string;
   articleSubtitle: string;
   url: string;
@@ -23,26 +28,35 @@ const rawVerificationMap: Record<string, string> = {
   false: "Falso",
 };
 
-const [DEFAULT_WIDTH, DEFAULT_HEIGHT] = [800, 400];
+const [DEFAULT_WIDTH, DEFAULT_HEIGHT] = [1200, 400];
 const RADIUS = 10;
 const MAX_APP_WIDTH = 1280;
 const MIN_APP_WIDTH = 320;
 const APP_X_PADDING = 0;
+
+const parseTime = d3.utcParse("%Y.%m.%d");
 
 const baseData = data.map((d) => ({
   ...d,
   x: DEFAULT_WIDTH / 2,
   y: DEFAULT_HEIGHT / 2,
 }));
+const dateExtent = [parseTime("2022.04.19")!, parseTime("2022.08.29")!];
 
 const colorScale = d3.schemeRdYlBu[5];
-const values = ["false", "creative", "misleading", "inaccurate", "true"];
+const verificationValues = [
+  "false",
+  "creative",
+  "misleading",
+  "inaccurate",
+  "true",
+];
 const getColor = (v: string) => {
-  const index = values.indexOf(v);
+  const index = verificationValues.indexOf(v);
   return index > -1 ? colorScale[index] : "black";
 };
 const getAvatar = (v: string) => {
-  const index = values.indexOf(v);
+  const index = verificationValues.indexOf(v);
   return index > -1
     ? `https://factchecking.cl/wp-content/themes/gauge-child/lib/images/faces/${
         5 - index
@@ -50,8 +64,32 @@ const getAvatar = (v: string) => {
     : "";
 };
 
+const sourceValues = [
+  "Instagram",
+  "TikTok",
+  "Twitter",
+  "YouTube",
+  "Facebook",
+  "leaflets",
+  "webpage",
+  "online media",
+  "television",
+  "electoral campaign",
+];
+
+type Scale = "verification" | "source" | "time";
+
+const fieldAccessor = (d: Node, scale: Scale) =>
+  (scale === "verification"
+    ? d.verification
+    : scale === "time"
+    ? d.date
+    : d.source?.medium ?? d.source?.platform) ?? "";
+
 function App() {
   const [nodes, setNodes] = useState<Node[]>(baseData);
+
+  const [scale, setScale] = useState<Scale>("verification");
 
   const size = useWindowSize();
   const orientation = (size.width ?? 1000) > 800 ? "horizontal" : "vertical";
@@ -79,16 +117,57 @@ function App() {
         };
   const sizeRef = useRef({ width: 0, height: 0 });
 
-  // console.log(size.width, size.height, dimensions.width, dimensions.height);
-  console.log({ nodes: [...nodes] });
+  const verificationScale = d3
+    .scaleBand(verificationValues, [
+      orientation === "horizontal" ? dimensions.width : dimensions.height,
+      0,
+    ])
+    .paddingOuter(0.1)
+    .paddingInner(0.2);
 
-  const positionScale =
-    orientation === "horizontal"
-      ? d3.scaleBand(values, [dimensions.width, 0]).padding(0.3)
-      : d3.scaleBand(values, [dimensions.height, 0]).padding(0.3);
+  const sourceScale = d3
+    .scaleBand(sourceValues, [
+      orientation === "horizontal" ? dimensions.width : dimensions.height,
+      0,
+    ])
+    .paddingOuter(0.1)
+    .paddingInner(0.2);
+
+  const timeScale = d3.scaleTime(dateExtent, [
+    (verificationScale(verificationValues[verificationValues.length - 2]) ??
+      0) +
+      verificationScale.bandwidth() / 2,
+    (verificationScale(verificationValues[0]) ?? 0) +
+      verificationScale.bandwidth() / 2,
+  ]);
+
   const getPosition = useCallback(
-    (v: string) => (positionScale(v) ?? 0) + positionScale.bandwidth() / 2,
-    [positionScale]
+    (v: string) => {
+      if (scale === "time") {
+        return !v
+          ? (verificationScale(
+              verificationValues[verificationValues.length - 1]
+            ) ?? 0) +
+              verificationScale.bandwidth() / 2
+          : timeScale(parseTime(v)!);
+      }
+      if (scale === "source") {
+        return (
+          (sourceScale(v) ?? sourceScale.range()[0]) +
+          sourceScale.bandwidth() / 2
+        );
+      }
+
+      if (scale === "verification") {
+        return (
+          (verificationScale(v) ?? verificationScale.range()[0]) +
+          verificationScale.bandwidth() / 2
+        );
+      }
+
+      return 0;
+    },
+    [scale, timeScale, sourceScale, verificationScale]
   );
 
   const simulation = useRef<d3.Simulation<Node, undefined>>(null);
@@ -103,18 +182,21 @@ function App() {
         .force(
           "forceX",
           orientation === "horizontal"
-            ? d3.forceX((d: Node) => getPosition(d.verification)).strength(0.1)
+            ? d3
+                .forceX((d: Node) => getPosition(fieldAccessor(d, scale)))
+                .strength(0.09)
             : d3.forceX(dimensions.width * 0.6).strength(0.05)
         )
         .force(
           "forceY",
           orientation === "horizontal"
             ? d3.forceY(dimensions.height * 0.6).strength(0.05)
-            : d3.forceY((d: Node) => getPosition(d.verification)).strength(0.1)
+            : d3
+                .forceY((d: Node) => getPosition(fieldAccessor(d, scale)))
+                .strength(0.09)
         )
-        .force("collision", d3.forceCollide(RADIUS * 1.2))
+        .force("collision", d3.forceCollide(RADIUS * 1.5))
         .on("tick", () => {
-          // console.log("tick", simulation.current?.alpha());
           if ((simulation.current?.alpha() ?? 1) < 0.01) {
             simulation.current?.stop();
           }
@@ -136,6 +218,7 @@ function App() {
       setNodes,
       dimensions.width,
       dimensions.height,
+      scale,
     ]
   );
 
@@ -156,6 +239,13 @@ function App() {
     };
   }, [dimensions.width, dimensions.height, getSimulation]);
 
+  useEffect(() => {
+    console.log("useEffect 2");
+
+    simulation.current?.stop();
+    simulation.current = getSimulation();
+  }, [scale]);
+
   return (
     <>
       <h1 style={{ maxWidth: "18ch" }}>
@@ -171,9 +261,15 @@ function App() {
         </p>
       </div>
 
+      <select value={scale} onChange={(e) => setScale(e.target.value as Scale)}>
+        <option value="verification">Verification</option>
+        <option value="source">Source</option>
+        <option value="time">Time</option>
+      </select>
+
       <div
         style={{
-          width: `${dimensions.width}px`,
+          width: `100%`,
           height: `${dimensions.height}px`,
           position: "relative",
           marginTop: "100px",
@@ -225,7 +321,7 @@ function App() {
                       fontSize: "12px",
                     }}
                   >
-                    <strong>{v.verificationRaw} </strong>
+                    <strong>{v.verificationRaw}</strong>
                     <img
                       style={{ width: "30px", height: "30px" }}
                       src={getAvatar(v.verification)}
@@ -233,7 +329,9 @@ function App() {
                   </p>
 
                   <p>{v.articleSubtitle}</p>
+                  <p>{v.date == null ? "Fecha desconocida" : v.date}</p>
                   <p>
+                    {v.source?.medium ?? v.source?.platform} -
                     <a href={v.url} rel="noreferrer" target="_blank">
                       Fuente
                     </a>
@@ -244,38 +342,69 @@ function App() {
           </Tooltip.Provider>
         ))}
 
-        {values.map((v) => (
-          <p
-            key={"veri-" + v}
-            style={{
-              backgroundColor: getColor(v),
-              color: "black",
-              display: "inline-flex",
-              alignItems: "center",
-              alignSelf: "end",
-              marginTop: "-20px",
-              padding: "3px",
-              borderRadius: "5px",
-              fontSize: "12px",
-              position: "absolute",
-              top:
-                orientation === "horizontal"
-                  ? `${20}px`
-                  : `${getPosition(v)}px`,
-              left:
-                orientation === "horizontal"
-                  ? `${getPosition(v)}px`
-                  : `${20}px`,
-              transform: orientation === "horizontal" ? `translateX(-50%)` : "",
-            }}
-          >
-            <strong>{rawVerificationMap[v]} </strong>
-            <img
-              style={{ width: "30px", height: "30px" }}
-              src={getAvatar(v)}
-            ></img>
-          </p>
-        ))}
+        {scale === "verification" &&
+          verificationValues.map((v) => (
+            <p
+              key={"veri-" + v}
+              style={{
+                backgroundColor: getColor(v),
+                color: "black",
+                display: "inline-flex",
+                alignItems: "center",
+                alignSelf: "end",
+                marginTop: "-20px",
+                padding: "3px",
+                borderRadius: "5px",
+                fontSize: "12px",
+                position: "absolute",
+                top:
+                  orientation === "horizontal"
+                    ? `${20}px`
+                    : `${getPosition(v)}px`,
+                left:
+                  orientation === "horizontal"
+                    ? `${getPosition(v)}px`
+                    : `${20}px`,
+                transform:
+                  orientation === "horizontal" ? `translateX(-50%)` : "",
+              }}
+            >
+              {rawVerificationMap[v]}
+              <img
+                style={{ width: "30px", height: "30px" }}
+                src={getAvatar(v)}
+              ></img>
+            </p>
+          ))}
+
+        {scale === "source" &&
+          sourceValues.map((v) => (
+            <p
+              key={"source-" + v}
+              style={{
+                display: "inline-flex",
+                alignItems: "center",
+                alignSelf: "end",
+                marginTop: "-20px",
+                padding: "3px",
+                borderRadius: "5px",
+                fontSize: "12px",
+                position: "absolute",
+                top:
+                  orientation === "horizontal"
+                    ? `${20}px`
+                    : `${getPosition(v)}px`,
+                left:
+                  orientation === "horizontal"
+                    ? `${getPosition(v)}px`
+                    : `${20}px`,
+                transform:
+                  orientation === "horizontal" ? `translateX(-50%)` : "",
+              }}
+            >
+              {v}
+            </p>
+          ))}
       </div>
     </>
   );
